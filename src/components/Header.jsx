05cba0a156a82cbee77778ac88caf84c7d1db07e
Mim-rose <mimafrose99@gmail.com>
2026-01-24 18:45:07 +0600

    Update Header component and add resume PDF

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,7 +60,7 @@ export default function Header() {
         </a>
 
         <a 
-          href="/assets/Jannatul_Afrose_Mim.pdf" 
+          href="/assets/Jannatul_Afrose_Mim_Resume(1).pdf" 
           download 
           className="px-10 py-3 border-2 border-gray-500 rounded-full flex items-center gap-2 hover:bg-gray-50 transition-all"
         >
